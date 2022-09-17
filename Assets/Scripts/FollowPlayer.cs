299968b07dc88f9422c1584b1f76a7fb019debb0
astasovsky <stasovsky.ua@gmail.com>
2022-09-17 20:19:15 +0300

    Task 1.3 High Speed Chase

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    private readonly Vector3 _offset = new Vector3(0, 5, -7);
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + _offset;
+    }
+}
